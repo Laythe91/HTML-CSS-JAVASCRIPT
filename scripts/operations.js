bd419b235971efab1f058d01b1cc71f818b33ef2
Laythe91 <109483760+Laythe91@users.noreply.github.com>
2025-01-30 07:37:55 +0100

    Projet initial

---
+++ scripts/operations.js
@@ -0,0 +1,20 @@
+var entier;
+entier = 10;
+
+var reel = 10.45;
+
+console.log(typeof entier, entier);
+console.log(typeof reel, reel);
+
+let chaineDeCaracteres = "Pecore sauvage";
+console.log(typeof chaineDeCaracteres,chaineDeCaracteres);
+
+const PI=3.14159;
+console.log(typeof PI, PI);
+
+const ISTRAINNER = true;
+console.log(typeof ISTRAINNER, ISTRAINNER);
+
+let numbers = [10,3,4,45];
+console.log(typeof numbers, numbers);
+
