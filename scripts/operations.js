21cc8b040bedc93efc650c1bf24f57be5d2962af
Laythe91 <109483760+Laythe91@users.noreply.github.com>
2025-01-30 08:25:23 +0100

    les commentaires et la mise en forme

--- scripts/operations.js
+++ scripts/operations.js
@@ -1,3 +1,11 @@
+/*
+    Formation Javascript
+    Présentation des types de données
+    var(DEPRECATED)
+    let pour déclarer des variables
+    const pour déclarer des constantes
+*/
+
 var entier;
 entier = 10;
 
@@ -7,14 +15,14 @@ console.log(typeof entier, entier);
 console.log(typeof reel, reel);
 
 let chaineDeCaracteres = "Pecore sauvage";
-console.log(typeof chaineDeCaracteres,chaineDeCaracteres);
+console.log(typeof chaineDeCaracteres, chaineDeCaracteres);
 
-const PI=3.14159;
+const PI = 3.14159;
 console.log(typeof PI, PI);
 
 const ISTRAINNER = true;
 console.log(typeof ISTRAINNER, ISTRAINNER);
 
+// Tableau de nombre
 let numbers = [10, 3, 4, 45];
 console.log(typeof numbers, numbers);
-
