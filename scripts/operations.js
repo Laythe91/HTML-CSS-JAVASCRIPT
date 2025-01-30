f3d3eaaf675b5ff0825126c10dc6f997ca239804
Laythe91 <109483760+Laythe91@users.noreply.github.com>
2025-01-30 08:40:15 +0100

    liste des opérations

--- scripts/operations.js
+++ scripts/operations.js
@@ -1,28 +1,13 @@
-/*
-    Formation Javascript
-    Présentation des types de données
-    var(DEPRECATED)
-    let pour déclarer des variables
-    const pour déclarer des constantes
-*/
+let resultat = 0;
+const FIRSTNUMBER = 10;
+const SECONDNUMBER = 20;
 
-var entier;
-entier = 10;
+resultat = FIRSTNUMBER + SECONDNUMBER;
 
-var reel = 10.45;
+// le resultat doit être 30
+console.log("La somme ", resultat);
 
-console.log(typeof entier, entier);
-console.log(typeof reel, reel);
+resultat = FIRSTNUMBER * SECONDNUMBER;
 
-let chaineDeCaracteres = "Pecore sauvage";
-console.log(typeof chaineDeCaracteres, chaineDeCaracteres);
-
-const PI = 3.14159;
-console.log(typeof PI, PI);
-
-const ISTRAINNER = true;
-console.log(typeof ISTRAINNER, ISTRAINNER);
-
-// Tableau de nombre
-let numbers = [10, 3, 4, 45];
-console.log(typeof numbers, numbers);
+// le resultat doit être 200
+console.log("Le produit ", resultat);
