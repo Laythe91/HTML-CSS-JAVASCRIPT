76e0b1636a24fe00e8f6374d24c604e7d5472e4c
Laythe91 <109483760+Laythe91@users.noreply.github.com>
2025-01-31 03:55:23 +0100

    rajout du fichier CSS et mise en place du TP

--- scripts/operations.js
+++ scripts/operations.js
@@ -5,9 +5,33 @@ function calculer() {
 
   const SecondNumberInput = document.getElementById("secondNumber");
   const valueOfSecondNumberInput = SecondNumberInput.value;
+  const operationChoisiInput = document.getElementById("monSelect");
+  const operationChoisi = Number(operationChoisiInput.value);
 
-  let resultat =
-    Number(valueOfFirstNumberInput) + Number(valueOfSecondNumberInput);
+  let resultat;
+  switch (operationChoisi) {
+    case 0:
+      resultat = "Choisissez une opération !!";
+      break;
+
+    case 1:
+      resultat =
+        Number(valueOfFirstNumberInput) + Number(valueOfSecondNumberInput);
+      break;
+    case 2:
+      resultat =
+        Number(valueOfFirstNumberInput) - Number(valueOfSecondNumberInput);
+      break;
+    case 3:
+      resultat =
+        Number(valueOfFirstNumberInput) * Number(valueOfSecondNumberInput);
+      break;
+    case 4:
+      resultat =
+        Number(valueOfFirstNumberInput) / Number(valueOfSecondNumberInput);
+      break;
+  }
+
   console.log(resultat);
 
   document.getElementById("result").innerHTML = resultat;
